5ff0406391ee2214cf6d5a4dc93fdaa31086dad3
takutouehara <138412475+takutouehara@users.noreply.github.com>
2023-07-10 12:09:51 +0900

    Update1
    Update
    2023/07/10

--- GameProject/Match3/AppMain.cpp
+++ GameProject/Match3/AppMain.cpp
@@ -1,21 +1,94 @@
+/*******************************
+*プログラミング実習
+* マッチ３ゲームの制作
+*******************************/
 #include "Dxlib.h"
+#include "FreamControl.h"
+#include "InputControl.h"
+#include "SceneManager.h"
+
+/*******************************
+*マクロ定義
+*******************************/
+#define SCREEN_HEIGHT	(480)	//スクリーンサイズ(高さ)
+#define SCREEN_WIDTH	(640)	//スクリーンサイズ(幅)
+#define SCREEN_COLORBIT (32)	//スクリーンカラービット
+#define FONT_SIZE		(20)	//文字サイズ
+
+/*******************************
+*型定義
+*******************************/
+
+/*******************************
+*グローバル変数宣言
+*******************************/
+
+/*******************************
+*プロトタイプ宣言
+*******************************/
+
+/*******************************
+*プログラムの開始
+*******************************/
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
+	//ウィンドウタイトル設定
+	SetMainWindowText("Match 3 Puzzle");
+
 	//ウィンドウモードで起動
 	ChangeWindowMode(TRUE);
 
+	//画面サイズの最大サイズ、カラービット数を設定
+	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_COLORBIT);
+
 	//DXライブラリの初期化処理
-	if (DxLib_Init() == -1)
+	//エラーが発生したら、終了する
+	if (DxLib_Init() == D_ERROR)
 	{
-		return -1;
+		return D_ERROR;
+	}
+
+	//各機能の初期化処理
+	FreamControl_Initialize();	//フレームレート制限機能
+	Input_Initialize();			//入力制限機能
+	
+	//シーンマネージャー初期化処理
+	//エラーが発生したら、終了する
+	if (SceneManager_INitialize(E_TITLE) == D_ERROR)
+	{
+		return D_ERROR;
 	}
 	
-	//入力待機
-	WaitKey();
+	//描画先画面を裏にする
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	//文字サイズを設定
+	SetFontSize(FONT_SIZE);
+
+	//ゲームループ
+	while (ProcessMessage() != D_ERROR && Input_Escape() == FALSE)
+	{
+		//入力制限機能更新処理
+		Input_Updete();
+
+		//シーンマネージャー更新処理
+		SceneManager_Update();
+
+		//画面クリア
+		ClearDrawScreen();
+
+		//シーンマネージャー描画処理
+		SceneManager_Draw();
+
+		//フレームレート制御処理
+		FreamControl_Update();
+
+		//画面の内容を表画面に反映
+		ScreenFlip;
+	}
 
 	//Dxライブラリ処理の終了処理
 	DxLib_End();
 
 	return 0;
-
 }
--- GameProject/Match3/Stage.h
+++ GameProject/Match3/Stage.h
@@ -1,1 +1,27 @@
 #pragma once
+
+/*******************************
+*マクロ定義
+*******************************/
+
+/*******************************
+*型定義
+*******************************/
+
+/*******************************
+*プロトタイプ宣言
+*******************************/
+int StageInitialize(void);
+void CreateBlock(void);
+void SelectBlock(void);
+void StageDraw(void);
+void FadeOutBlock(void);
+void MoveBlock(void);
+void CheckBlock(void);
+void CheckClear(void);
+
+int Get_StageState(void);
+int Get_StageClearFlag(void);
+int Get_StageScore(void);
+
+void Set_StageMission(int mission);
