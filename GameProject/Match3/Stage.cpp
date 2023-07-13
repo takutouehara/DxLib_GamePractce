400254806cbe25611e6034b0642c076600ffcae2
takutouehara <138412475+takutouehara@users.noreply.github.com>
2023-07-13 12:08:29 +0900

    Update3
    Update3

--- GameProject/Match3/Stage.cpp
+++ GameProject/Match3/Stage.cpp
@@ -76,6 +76,114 @@ void restore_block(void);
 *******************************/
 int StageInitialize(void)
 {
+	int ret = 0;
+	int i;
 
+	//画像読み込み
+	LoadDivGraph("images/block.png", BLOCK_IMAGE_MAX, BLOCK_IMAGE_MAX, 1, BLOCKSIZE, BLOCKSIZE, BlockImage);
+	StageImage = LoadGraph("images/stage.png");
+	//音源読み込み
+	ClickSE = LoadSoundMem("sounds/click_se.mp3");
+	FadeOutSE = LoadSoundMem("sounds/fadeout_se.mp3");
+	MoveBlockSE = LoadSoundMem("sounds/moveblock_se.mp3");
+
+	//ブロック生成処理
+	CreateBlock();
+
+	ClickStatus = E_NONE;
+	Stage_State = 0;
+	Stage_Score = 0;
+	ClearFlag = FALSE;
+
+	for (i = 0; i < 3; i++)
+	{
+		Select[i].x = 0;
+		Select[i].y = 0;
+	}
+	
+	//エラーチェック
+	for (i = 0; i < BLOCK_IMAGE_MAX; i++)
+	{
+		if (BlockImage[i] == -1)
+		{
+			ret = -1;
+			break;
+		}
+	}
+
+	if (StageImage == -1)
+	{
+		ret = -1;
+	}
+	if (ClickSE == -1)
+	{
+		ret = -1;
+	}
+	if (FadeOutSE == -1)
+	{
+		ret = -1;
+	}
+	if (MoveBlockSE == -1)
+	{
+		ret = -1;
+	}
+
+	return ret;
 }
 
+/*******************************
+*ステージ制御機能：ステージの描画
+* 引数：なし
+* 戻り値：なし
+*******************************/
+void StageDraw(void)
+{
+	DrawGraph(0,0,StageImage,FALSE);
+
+	//アイテムの取得個数を描画
+	for (int i = 0; i < ITEM_MAX; i++)
+	{
+		DrawRotaGraph(540, 245 + i * 30, 0.5f, 0, BlockImage[i + 1], TRUE, 0);
+		DrawFormatString(580, 235 + i * 30, 0xffffff, "%3d", Item[i]);
+	}
+
+	//ブロックを描画
+	for (int i = 0; i < HEIGHT; i++)
+	{
+		for (int j = 0; j < WIDTH; j++)
+		{
+			if (Block[i][j].flg == TRUE && Block[i][j].image != NULL)
+			{
+				DrawGraph(Block[i][j].x, Block[i][j].y, BlockImage[Block[i][j].image], TRUE);
+			}
+		}
+	}
+
+	//選択ブロックを描画
+	DrawGraph(Select[SELECT_CURSOR].x * BLOCKSIZE, Select[SELECT_CURSOR].y * BLOCKSIZE, BlockImage[9], TRUE);
+	if (ClickStatus != E_NONE)
+	{
+		DrawGraph(Select[NEXT_CURSOR].x * BLOCKSIZE, Select[NEXT_CURSOR].y * BLOCKSIZE, BlockImage[9], TRUE);
+	}
+
+	//ミッションを描画
+	SetFontSize(20);
+	DrawFormatString(590, 211, GetColor(255, 255, 255), "%3d", Stage_Mission);
+	
+	//アイテムの取得個数を描画
+	for (int i = 0; i < ITEM_MAX; i++)
+	{
+		DrawRotaGraph(540, 245 + i * 30, 0.5f, 0, BlockImage[i + 1], TRUE, 0);
+		DrawFormatString(580, 235 + i * 30, GetColor(255, 255, 255), "%3d", Item[i]);
+	}
+}
+
+/*******************************
+*ステージ制御機能：ブロック生成処理
+* 引数：なし
+* 戻り値：なし
+*******************************/
+void CreateBlock(void)
+{
+
+}
